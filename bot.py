a76d82fd83dfc923c120ac99190d1c4e3efb3453
Wizard21343 <saidolimsalikhodjaev2@gmail.com>
2026-05-05 01:00:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 from docx import Document
 
-import os
+
 
 TOKEN = os.getenv("TOKEN")
 
